b87371090612be6892c95f83041f286efb6503ff
syeon012 <seohyun081102.4@gmail.com>
2026-07-13 15:41:25 +0900

    Camera

--- Assets/Script-FGgreen/PlayerMove.cs
+++ Assets/Script-FGgreen/PlayerMove.cs
@@ -6,7 +6,6 @@ public class PlayerMove : MonoBehaviour
     private Vector2 MoveInput;
     private Rigidbody2D rb;
     public float MoveSpeed = 5f;
-    public bool Moveable = true;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -20,7 +19,6 @@ public class PlayerMove : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (!Moveable) return;
         rb.linearVelocity = new Vector2(MoveInput.x * MoveSpeed, MoveInput.y*MoveSpeed);
     }
 }
