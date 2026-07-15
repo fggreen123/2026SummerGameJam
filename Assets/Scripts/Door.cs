42d91f2b59d79ca17bd95eca655d93620a82ce85
glassmushroom <sgnjhsvvgjs@gmail.com>
2026-07-15 11:40:27 +0900

    Door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+    public string nextSceneName;
+
+    public string enemyTag = "Enemy";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            int enemyCount = GameObject.FindGameObjectsWithTag(enemyTag).Length;
+
+            if(enemyCount == 0)
+            {
+                Debug.Log("모든 적 처치 완료. 다음 맵 이동");
+                SceneManager.LoadScene(nextSceneName);
+            }
+            else
+            {
+                Debug.Log($"아직 적이 {enemyCount}마리 있어 문이 열리지 않습니다!");
+            }
+        }
+    }
+}
