57f8053c7c5a57d2986a671d5bf77e50662e5d4b
Bini0403 <lyb100403@naver.com>
2026-07-15 10:25:23 +0900

    Audio, TitleScene, Scripts
    Audio, TitleScene, Scripts

---
+++ Assets/Scripts/TitleUI.cs
@@ -0,0 +1,37 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleUI : MonoBehaviour
+{
+    public GameObject titlePanel;
+    public GameObject settingPanel;
+
+    void Start()
+    {
+        titlePanel.SetActive(true);
+        settingPanel.SetActive(false);
+    }
+
+    public void OpenSetting()
+    {
+        titlePanel.SetActive(false);
+        settingPanel.SetActive(true);
+    }
+
+    public void CloseSetting()
+    {
+        settingPanel.SetActive(false);
+        titlePanel.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!!!!!!!!!!!!!!!!!!!!!!!!");
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Test");
+    }
+}
